8415d14025f855957edcd37d9ff0f9ae7b99034d
Justin Sainton <justinsainton@gmail.com>
2013-09-03 09:38:32 -0500

    Resizing panels dynamically, more map work.

--- js/site.js
+++ js/site.js
@@ -1,26 +1,18 @@
-function initialize() {
-	var mapOptions = {
-		zoom: 8,
-		center: new google.maps.LatLng(-34.397, 150.644),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-
-	var map = new google.maps.Map(document.getElementById( 'map-canvas' ),
-	mapOptions);
-}
-
-function loadScript() {
-	var script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-	'callback=initialize';
-	document.body.appendChild(script);
-}
-
-window.onload = loadScript;
-
 (function($) {
+
+	/** Panel Resizing **/
+	resize_panels = function() {
+		var bodyheight = $( window ).height();
+		$( 'div.panel' ).height( bodyheight );
+	}
+
+	resize_panels();
+
+	$ ( window ).resize( resize_panels );
+
+	/** Video Background **/
 	var BV;
+
 	$(function() {
 		// initialize BigVideo
 		BV = new $.BigVideo();
@@ -32,5 +24,42 @@ window.onload = loadScript;
 		}
 
 	});
+
+	/** Google Maps **/
+	var map;
+
+	function initialize() {
+		google.maps.visualRefresh = true;
+
+		var hq    = new google.maps.LatLng( 42.680964,-87.812026 ),
+		recording = new google.maps.LatLng( 42.580899,-87.848738 ),
+		tv        = new google.maps.LatLng( 42.723704,-87.789871 ),
+		mapOptions = {
+			zoom        : 12,
+			center      : hq,
+			mapTypeId   : google.maps.MapTypeId.ROADMAP,
+			scrollwheel : false
+		},
+		image = 'img/map-marker.png',
+		hqMarker = new google.maps.Marker({
+			position : hq,
+			map      : map,
+			icon     : image
+		}),
+		recMarker = new google.maps.Marker({
+			position : recording,
+			map      : map,
+			icon     : image
+		}),
+		tvMarker = new google.maps.Marker({
+			position : tv,
+			map      : map,
+			icon     : image
+		}),
+
+		map = new google.maps.Map( document.getElementById( 'panel-5' ), mapOptions );
+	}
+
+	google.maps.event.addDomListener( window, 'load', initialize );
 
 })(jQuery);
