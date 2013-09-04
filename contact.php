8f67b2470c31fcc382e45673ef7657258bfee859
Justin Sainton <justinsainton@gmail.com>
2013-09-03 21:25:32 -0500

    Minor changes to fix broken stuff.

--- contact.php
+++ contact.php
@@ -10,11 +10,11 @@ if ( isset( $_POST ) ) {
 
 	$name    = filter_var( $_POST['name']    , FILTER_SANITIZE_STRING );
 	$email   = filter_var( $_POST['email']   , FILTER_SANITIZE_EMAIL );
-	$message = filter_var( $_POST['message'] , FILTER_SANITIZE_STRING );
+	$message = stripslashes( $_POST['message'] );
 
 	$to      = 'justinsainton@gmail.com';
 	$subject = 'Message from Contact Form on SkiesFall.com';
-	$headers = "From: $name <$email>\r\n";
+	$headers = "From: " . $name . " <" . $email . ">" . "\r\n";
 
 	if ( mail( $to, $subject, $message, $headers ) ) {
 		die( json_encode( array( 'success' => 'Thanks for getting in touch.' ) ) );
