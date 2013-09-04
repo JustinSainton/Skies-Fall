a6436e09030b0c063e30931657aeb1f832d33162
Justin Sainton <justinsainton@gmail.com>
2013-09-03 21:41:59 -0500

    Add email validation.

--- contact.php
+++ contact.php
@@ -8,6 +8,9 @@ if ( isset( $_POST ) ) {
 	if ( empty( $_POST['name'] ) || empty( $_POST['email'] ) || empty( $_POST['message'] ) )
 		die( json_encode( array( 'errormsg' => 'All fields are required.' ) ) );
 
+	if ( ! filter_var( $_POST['email']   , FILTER_VALIDATE_EMAIL ); )
+		die( json_encode( array( 'errormsg' => "That doesn't appear to be a valid email address." ) ) );
+
 	$name    = filter_var( $_POST['name']    , FILTER_SANITIZE_STRING );
 	$email   = filter_var( $_POST['email']   , FILTER_SANITIZE_EMAIL );
 	$message = stripslashes( $_POST['message'] );
