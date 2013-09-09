bde644d1ef5668ed02ef4c2e9577b3c046c8d65e
Justin Sainton <justinsainton@gmail.com>
2013-09-08 20:37:47 -0700

    So, scrollable and vertical classes are important

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
 					</li>
 				</ul>
 			</nav>
-			<div class="panel-container" data-stellar-ratio=".75">
+			<div class="panel-container scrollable vertical" data-stellar-ratio=".75">
 				<span class="heading">We own the sky</span>
 				<p class="quote">We are artists, sound &amp; visual designers, photographers, filmmakers, and business strategists. Seekers of beauty &amp; innovation.<br />Bound together by our common ambition to engage culture in a pursuit of excellence, creativity, and authentic connection.</p>
 			</div>
