4f5697c263b4e29495d179bdb16129374226574d
Justin Sainton <justinsainton@gmail.com>
2013-09-14 12:13:03 -0700

    More tweaking on height.  Note: At the moment, it seems like the overflow scroll height on the scroll space here is a bit faulty on HD large screens. Need to investigate

--- js/site.js
+++ js/site.js
@@ -349,7 +349,7 @@ jQuery( document ).ready( function( $ ) {
 		//Print
 		$( 'div.print' ).css( 'min-height', Math.round( window.innerWidth * .5375 ) );
 		$( 'div.print div.scroll-space' ).css( 'width', Math.round( window.innerWidth * 0.21614583333333334 ) );
-		$( 'div.print div.scroll-space' ).css( 'height', Math.round( window.innerWidth * 0.43645833333333334 ) );
+		$( 'div.print div.scroll-space' ).css( 'height', Math.round( window.innerWidth * 0.43333333333333335 ) );
 		$( 'div.print div.scroll-space' ).css( 'top', Math.round( window.innerWidth * 0.057291666666666664 ) );
 		$( 'div.print div.scroll-space' ).css( 'left', Math.round( window.innerWidth * 0.5682291666666667 ) );
 
