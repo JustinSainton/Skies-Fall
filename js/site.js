eefb205688b01a6a1e7dd4e8b135698113c00cc8
Justin Sainton <justinsainton@gmail.com>
2013-09-14 11:58:14 -0700

    First pass at fixing unexpected identifier error

--- js/site.js
+++ js/site.js
@@ -353,7 +353,9 @@ jQuery( document ).ready( function( $ ) {
 		$( 'div.print div.scroll-space' ).css( 'top', Math.round( window.innerWidth * 0.057291666666666664 ) );
 		$( 'div.print div.scroll-space' ).css( 'left', Math.round( window.innerWidth * 0.5682291666666667 ) );
 
-		var screenImage = $( 'div.print img' ).eq(0), theImage = new Image(), theImage.src = screenImage.attr( 'src' );
+		var screenImage = $( 'div.print img' ).eq(0),
+		theImage = new Image(),
+		theImage.src = screenImage.attr( 'src' );
 
 		$( 'div.print img' ).css( 'width', Math.ceil( theImage.width * ( window.innerWidth / theImage.width ) ) );
 
