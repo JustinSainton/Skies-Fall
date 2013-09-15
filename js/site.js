e8605e77be30f9e631dcaeb27fb438d3616225fe
Justin Sainton <justinsainton@gmail.com>
2013-09-14 20:17:17 -0700

    We're going responsive y'all

--- js/site.js
+++ js/site.js
@@ -377,17 +377,6 @@ jQuery( document ).ready( function( $ ) {
 
 	$( window ).resize( size_design_scrollers );
 
-	var place_audio_paragraphs = function() {
-		$( 'body.audio #panel-3 div.grid-item p' ).css( 'width', Math.round( window.innerWidth * 0.24479166666666666 ) );
-		$( 'body.audio #panel-3 div.grid-item p' ).css( 'top', Math.round( window.innerWidth * 0.19375 ) );
-		$( 'body.audio #panel-3 div.grid-item p' ).css( 'left', Math.round( window.innerWidth * 0.234375 ) );
-		$( 'body.audio #panel-3 div.grid-item:odd p' ).css( 'left', Math.round( window.innerWidth * 0.052083333333333336 ) );
-	}
-
-	place_audio_paragraphs();
-
-	$( window ).resize( place_audio_paragraphs );
-
 	//Ensure girls and guys scroll at same time
 	if ( $( 'div.apparel div.scroll-space' ).length ) {
 		var scroll_difference = $( 'div.apparel div.scroll-space' ).get(1).scrollHeight / $( 'div.apparel div.scroll-space' ).get(0).scrollHeight;
